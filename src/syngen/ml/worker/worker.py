6d965791b5cbeb94dc363aee0c7271fb4b66f6de
Hanna Imshenetska <hanna_imshenetska@epam.com>
2022-11-04 11:32:52 +0200

    update the package 'ml/data_loaders', update the package 'ml/worker'

--- src/syngen/ml/worker/worker.py
+++ src/syngen/ml/worker/worker.py
@@ -1,5 +1,4 @@
 from typing import Dict, List, Optional
-from dataclasses import dataclass
 
 from loguru import logger
 
@@ -7,21 +6,21 @@ from syngen.ml.data_loaders import MetadataLoader
 from syngen.ml.interface import TrainInterface, InferInterface
 
 
-@dataclass
 class Worker:
     """
     Class for preparing training and infer settings, metadata for training and infer process
     """
-    table_name: str
-    metadata_path: Optional[str]
-    settings: Dict
-    train_interface = TrainInterface()
-    infer_interface = InferInterface()
-    metadata_loader = MetadataLoader()
-    metadata = None
+    def __init__(self, table_name: str, metadata_path: Optional[str], settings: Dict):
+        self.table_name = table_name
+        self.metadata_path = metadata_path
+        self.settings = settings
+        self.train_interface = TrainInterface()
+        self.infer_interface = InferInterface()
+        self.metadata_loader = MetadataLoader(self.metadata_path)
+        self.metadata = None
 
     def __post_init__(self):
-        self.metadata = self.metadata_loader.load_data(self.metadata_path) if self.metadata_path else None
+        self.metadata = self.metadata_loader.load_data() if self.metadata_path else None
 
     def __parse_train_settings(self, config: Dict):
         """
