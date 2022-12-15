c0a14ba42477d4844a90a1f95f26502977ca2c4c
Hanna Imshenetska <hanna_imshenetska@epam.com>
2022-12-15 19:40:34 +0200

    remove the method 'pipeline' from the package 'ml/pipeline'

--- src/syngen/ml/pipeline/pipeline.py
+++ src/syngen/ml/pipeline/pipeline.py
@@ -4,7 +4,6 @@ import pickle
 
 import pandas as pd
 import numpy as np
-from loguru import logger
 
 
 def get_date_columns(df: pd.DataFrame, str_columns: List[str]):
@@ -30,7 +29,8 @@ def get_date_columns(df: pd.DataFrame, str_columns: List[str]):
             return np.nan
 
     data_subset = df[str_columns]
-    data_subset = df if not df.empty else data_subset.loc[:, data_subset.apply(len_filter)]
+
+    data_subset = data_subset.loc[:, data_subset.apply(len_filter)]
     long_text_columns = data_subset.columns
     str_columns = [i for i in str_columns if i not in long_text_columns]
     date_columns = df[str_columns].apply(date_finder).dropna()
@@ -100,47 +100,6 @@ def get_tmp_df(df):
         if tmp_col_len < tmp_col_len_min:
             tmp_col_len_min = tmp_col_len
     return pd.DataFrame(tmp_cols).iloc[:tmp_col_len_min, :]
-
-
-def data_pipeline(df: pd.DataFrame, check_object_on_float: bool = False) -> Tuple:
-    if check_object_on_float:
-        columns_nan_labels = get_nan_labels(df)
-        df = nan_labels_to_float(df, columns_nan_labels)
-
-    binary_columns = set([col for col in df.columns if df[col].fillna("?").nunique() == 2])
-    categ_columns = set([col for col in df.columns if df[col].dropna().nunique() <= 50 and col not in binary_columns])
-    tmp_df = get_tmp_df(df)
-    float_columns = set(tmp_df.select_dtypes(include=["float", "float64"]).columns)
-    int_columns = set(tmp_df.select_dtypes(include=["int", "int64"]).columns)
-
-    float_to_int_cols = set()
-    for col in float_columns:
-        if all(x.is_integer() for x in tmp_df[col]):
-            float_to_int_cols.add(col)
-
-    int_columns = (int_columns | float_to_int_cols) - (categ_columns | binary_columns)
-    float_columns = float_columns - categ_columns - int_columns - binary_columns
-    str_columns = set(tmp_df.columns) - float_columns - categ_columns - int_columns - binary_columns
-    date_columns = get_date_columns(tmp_df, list(str_columns))
-    str_columns -= date_columns
-
-    assert len(str_columns) + \
-           len(float_columns) + \
-           len(int_columns) + \
-           len(date_columns) + \
-           len(categ_columns) + \
-           len(binary_columns) == len(df.columns), "According to number of columns with defined types, " \
-                                                   "column types are not identified correctly."
-
-    logger.debug(
-        f"Count of string columns: {len(str_columns)}; "
-        + f"Count of float columns: {len(float_columns)}; "
-        + f"Count of int columns: {len(int_columns)}; "
-        + f"Count of categorical columns: {len(categ_columns)}; "
-        + f"Count of date columns: {len(date_columns)}; "
-        + f"Count of binary columns: {len(binary_columns)}"
-    )
-    return str_columns, float_columns, categ_columns, date_columns, int_columns, binary_columns
 
 
 def fillnan(df, str_columns, float_columns, categ_columns):
