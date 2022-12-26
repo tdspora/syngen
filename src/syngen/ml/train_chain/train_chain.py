efda03814c8ba3c7991de1d8ac3ca2ffa98d595a
elizaveta.kapitonov <elizaveta.kapitonov@ihsmarkit.com>
2022-12-26 17:54:16 +0300

    fixed concatenation of batches in case of no keys

--- src/syngen/ml/train_chain/train_chain.py
+++ src/syngen/ml/train_chain/train_chain.py
@@ -296,7 +296,6 @@ class VaeInferHandler(BaseHandler):
             logger.info(f"Total of {batch_num} batch(es)")
             batches = self.split_by_batches(size, batch_num)
             prepared_batches = [self.run(batch, run_parallel) for batch in batches]
-            prepared_data = pd.DataFrame()
             prepared_data = self._concat_slices_with_unique_pk(prepared_batches) if len(prepared_batches) > 0 else pd.DataFrame()
 
             is_pk = self._is_pk()
