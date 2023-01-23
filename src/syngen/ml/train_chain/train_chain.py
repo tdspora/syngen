177236e9b32744ca3ee91717863aecd3150d5300
Pavel_Bobyrev <Grakachak25011994>
2023-01-23 18:54:30 +0100

    Handle several FKs

--- src/syngen/ml/train_chain/train_chain.py
+++ src/syngen/ml/train_chain/train_chain.py
@@ -242,10 +242,8 @@ class VaeInferHandler(BaseHandler):
             with open(f"{self.fk_kde_path}_{fk_label}.pkl", "rb") as file:
                 kde = dill.load(file)
             pk = pk.dropna()
-            if pk.dtype == "object":
-                fk_pdf = np.maximum(kde.evaluate(np.arange(len(pk))), 1e-12)
-            else:
-                fk_pdf = np.maximum(kde.evaluate(pk), 1e-12)
+            numeric_pk = np.arange(len(pk)) if pk.dtype == "object" else pk
+            fk_pdf = np.maximum(kde.evaluate(numeric_pk), 1e-12)
             synth_fk = np.random.choice(pk, size=size, p=fk_pdf / sum(fk_pdf), replace=True)
             synth_fk = pd.DataFrame({fk_label: synth_fk}).reset_index(drop=True)
 
