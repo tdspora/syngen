2aca85854439a3d0a9b6348da09f0fa543019c5e
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-01-27 18:01:54 +0200

    update 'ml/config', the template of Sample report

--- src/syngen/ml/config/configurations.py
+++ src/syngen/ml/config/configurations.py
@@ -144,7 +144,7 @@ class TrainConfig:
             "results_path": f"model_artifacts/tmp_store/{self.table_name}/metrics_{self.table_name}.csv",
             "bad_columns_path": f"model_artifacts/tmp_store/{self.table_name}/bad_columns_{self.table_name}.csv",
             "dataset_pickle_path": f"model_artifacts/resources/{self.table_name}/vae/checkpoints/model_dataset.pkl",
-            "fk_kde_path": f"model_artifacts/resources/{self.table_name}/vae/checkpoints/fk_kde.pkl",
+            "fk_kde_path": f"model_artifacts/resources/{self.table_name}/vae/checkpoints/",
             "original_data_path": f"model_artifacts/tmp_store/{self.table_name}/input_data_{self.table_name}.csv",
             "synthetic_data_path": f"model_artifacts/tmp_store/{self.table_name}/merged_infer_{self.table_name}.csv",
         }
@@ -203,5 +203,5 @@ class InferConfig:
             "tmp_store_path": f"model_artifacts/tmp_store/{dynamic_name}",
             "vae_resources_path": f"model_artifacts/resources/{dynamic_name}/vae/checkpoints/",
             "dataset_pickle_path": f"model_artifacts/resources/{dynamic_name}/vae/checkpoints/model_dataset.pkl",
-            "fk_kde_path": f"model_artifacts/resources/{dynamic_name}/vae/checkpoints/fk_kde.pkl"
+            "fk_kde_path": f"model_artifacts/resources/{dynamic_name}/vae/checkpoints/"
         }
