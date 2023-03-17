5205a8fc60a2dcb3f04c2c6c79edeccfb3ece396
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-03-17 13:36:44 +0200

    rename 'syngen/ml/utils/pipeline.py' to 'syngen/ml/utils/utils.py'

--- src/syngen/ml/utils/__init__.py
+++ src/syngen/ml/utils/__init__.py
@@ -1,8 +1,12 @@
-from syngen.ml.utils.pipeline import get_date_columns
-from syngen.ml.utils.pipeline import get_nan_labels
-from syngen.ml.utils.pipeline import nan_labels_to_float
-from syngen.ml.utils.pipeline import get_tmp_df
-from syngen.ml.utils.pipeline import fillnan
-from syngen.ml.utils.pipeline import fetch_dataset
-from syngen.ml.utils.pipeline import slugify_attribute
-from syngen.ml.utils.pipeline import slugify_parameters
+from syngen.ml.utils.utils import (
+    get_date_columns,
+    get_nan_labels,
+    nan_labels_to_float,
+    get_tmp_df,
+    fillnan,
+    fetch_dataset,
+    slugify_attribute,
+    slugify_parameters,
+    slugify_parameter_with_restriction,
+    inverse_dict
+)
