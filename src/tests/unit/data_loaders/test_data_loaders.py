87522c273be48ea5a4c0e983fe1fe9d423707d10
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-04-05 15:09:12 +0300

    minor changes in log message in 'ml/convertor', cover 'ml/convertor' by unit tests

--- src/tests/unit/data_loaders/test_data_loaders.py
+++ src/tests/unit/data_loaders/test_data_loaders.py
@@ -1,8 +1,8 @@
-import pandas as pd
+from unittest.mock import patch, mock_open
 import pytest
+import pandas as pd
 from pandas.testing import assert_frame_equal
 import os
-from unittest.mock import patch
 
 from syngen.ml.data_loaders import (
     DataLoader,
@@ -14,12 +14,12 @@ from syngen.ml.data_loaders import (
 )
 
 
-
 def test_initialize_data_loader_for_single_csv_table_with_existed_path():
     test_data_loader = DataLoader("tests/unit/data_loaders/fixtures/csv_tables/table_with_data.csv")
     assert test_data_loader.path == "tests/unit/data_loaders/fixtures/csv_tables/table_with_data.csv"
     assert test_data_loader.has_existed_path is True
     assert isinstance(test_data_loader.file_loader, CSVLoader)
+
 
 def test_initialize_data_loader_for_single_csv_table_with_not_existed_path():
     test_data_loader = DataLoader("path/to/table.csv")
@@ -27,11 +27,13 @@ def test_initialize_data_loader_for_single_csv_table_with_not_existed_path():
     assert test_data_loader.has_existed_path is False
     assert isinstance(test_data_loader.file_loader, CSVLoader)
 
+
 def test_initialize_data_loader_for_single_avro_table_with_existed_path():
     test_data_loader = DataLoader("tests/unit/data_loaders/fixtures/avro_tables/table_with_data.avro")
     assert test_data_loader.path == "tests/unit/data_loaders/fixtures/avro_tables/table_with_data.avro"
     assert test_data_loader.has_existed_path is True
     assert isinstance(test_data_loader.file_loader, AvroLoader)
+
 
 def test_initialize_data_loader_for_single_avro_table_with_not_existed_path():
     test_data_loader = DataLoader("path/to/table.avro")
@@ -39,11 +41,13 @@ def test_initialize_data_loader_for_single_avro_table_with_not_existed_path():
     assert test_data_loader.has_existed_path is False
     assert isinstance(test_data_loader.file_loader, AvroLoader)
 
+
 def test_initialize_data_loader_for_single_pickle_table_with_existed_path():
     test_data_loader = DataLoader("tests/unit/data_loaders/fixtures/pickle_tables/table_with_data.pkl")
     assert test_data_loader.path == "tests/unit/data_loaders/fixtures/pickle_tables/table_with_data.pkl"
     assert test_data_loader.has_existed_path is True
     assert isinstance(test_data_loader.file_loader, BinaryLoader)
+
 
 def test_initialize_data_loader_for_single_pickle_table_with_not_existed_path():
     test_data_loader = DataLoader("path/to/table.pkl")
@@ -51,16 +55,19 @@ def test_initialize_data_loader_for_single_pickle_table_with_not_existed_path():
     assert test_data_loader.has_existed_path is False
     assert isinstance(test_data_loader.file_loader, BinaryLoader)
 
+
 def test_initialize_data_loader_for_single_table_with_unsupported_format():
     with pytest.raises(NotImplementedError) as error:
         DataLoader("path/to/table.test")
         assert str(error.value) == "File format not supported"
+
 
 def test_initialize_data_loader_for_single_table_with_empty_path(caplog):
     with pytest.raises(ValueError):
         with caplog.at_level("ERROR"):
             DataLoader("")
         assert "It seems that the information of source is absent" in caplog.text
+
 
 def test_load_data_from_table_in_csv_format():
     data_loader = DataLoader("tests/unit/data_loaders/fixtures/csv_tables/table_with_data.csv")
@@ -79,6 +86,7 @@ def test_load_data_from_table_in_csv_format():
     assert isinstance(df, pd.DataFrame)
     assert schema == {"format": "CSV"}
 
+
 def test_load_data_from_empty_table_in_csv_format(caplog):
     data_loader = DataLoader("tests/unit/data_loaders/fixtures/csv_tables/empty_table.csv")
     assert isinstance(data_loader.file_loader, CSVLoader)
@@ -86,6 +94,7 @@ def test_load_data_from_empty_table_in_csv_format(caplog):
         with caplog.at_level("ERROR"):
             data_loader.load_data()
         assert "It seems that empty file was provided. Unable to train" in caplog.text
+
 
 def test_load_data_from_table_in_csv_format_in_not_utf_8(caplog):
     path = "tests/unit/data_loaders/fixtures/csv_tables/table_in_iso_encoding.csv"
@@ -99,6 +108,7 @@ def test_load_data_from_table_in_csv_format_in_not_utf_8(caplog):
                f"doesn't have the encoding UTF-8. The details of the error - {error}.\n" \
                f"Please, use the data in UTF-8 encoding" in caplog.text
 
+
 def test_save_data_in_csv_format(test_csv_path, test_df):
     data_loader = DataLoader(test_csv_path)
     data_loader.save_data(test_csv_path, test_df)
@@ -108,6 +118,7 @@ def test_save_data_in_csv_format(test_csv_path, test_df):
 
     loaded_df, schema = data_loader.load_data()
     pd.testing.assert_frame_equal(loaded_df, test_df)
+
 
 def test_load_data_from_table_in_avro_format():
     data_loader = DataLoader("tests/unit/data_loaders/fixtures/avro_tables/table_with_data.avro")
@@ -127,6 +138,7 @@ def test_load_data_from_table_in_avro_format():
     assert isinstance(df, pd.DataFrame)
     assert schema == {'fields': {'gender': 'int', 'height': 'float', 'id': 'int'}, 'format': 'Avro'}
 
+
 def test_load_data_from_empty_table_in_avro_format(caplog):
     path = "tests/unit/data_loaders/fixtures/avro_tables/empty_table.avro"
     data_loader = DataLoader(path)
@@ -138,6 +150,7 @@ def test_load_data_from_empty_table_in_avro_format(caplog):
             data_loader.load_data()
         assert "It seems that empty file was provided. Unable to train" in caplog.text
 
+
 def test_save_data_in_avro_format(test_avro_path, test_df):
     data_loader = DataLoader(test_avro_path)
     data_loader.save_data(test_avro_path, test_df)
@@ -147,6 +160,7 @@ def test_save_data_in_avro_format(test_avro_path, test_df):
 
     loaded_df, schema = data_loader.load_data()
     pd.testing.assert_frame_equal(loaded_df, test_df)
+
 
 def test_load_data_from_table_in_pickle_format():
     data_loader = DataLoader("tests/unit/data_loaders/fixtures/pickle_tables/table_with_data.pkl")
@@ -166,6 +180,7 @@ def test_load_data_from_table_in_pickle_format():
     assert isinstance(df, pd.DataFrame)
     assert schema is None
 
+
 def test_load_data_from_empty_table_in_pickle_format(caplog):
     path = "tests/unit/data_loaders/fixtures/pickle_tables/empty_table.pkl"
     data_loader = DataLoader(path)
@@ -176,6 +191,7 @@ def test_load_data_from_empty_table_in_pickle_format(caplog):
         with caplog.at_level("ERROR"):
             data_loader.load_data()
         assert "It seems that empty file was provided. Unable to train" in caplog.text
+
 
 def test_save_data_in_pickle_format(test_pickle_path, test_df):
     data_loader = DataLoader(test_pickle_path)
@@ -193,10 +209,12 @@ def test_initialize_metadata_loader():
     assert test_metadata_loader.metadata_path == "tests/unit/data_loaders/fixtures/metadata/metadata.yaml"
     assert isinstance(test_metadata_loader.metadata_loader, YAMLLoader)
 
+
 def test_initialize_metadata_loader_with_unsupported_format():
     with pytest.raises(NotImplementedError) as error:
         MetadataLoader("path/to/table.test")
         assert str(error.value) == "File format not supported"
+
 
 def test_load_metadata_in_yaml_format():
     path_to_metadata = "tests/unit/data_loaders/fixtures/metadata/metadata.yaml"
@@ -228,6 +246,7 @@ def test_load_metadata_in_yaml_format():
         }
     }
 
+
 def test_load_metadata_in_yml_format():
     path_to_metadata = "tests/unit/data_loaders/fixtures/metadata/metadata.yml"
     test_metadata_loader = MetadataLoader(path_to_metadata)
@@ -258,6 +277,85 @@ def test_load_metadata_in_yml_format():
         }
     }
 
+
+def test_load_metadata_by_yaml_loader_in_yaml_format():
+    loader = YAMLLoader()
+
+    expected_metadata = {
+        "pk_test": {
+            "train_settings": {
+                "drop_null": False,
+                "epochs": 1,
+                "print_report": False,
+                "row_limit": 800
+            },
+            "infer_settings": {
+                "print_report": True,
+                "random_seed": 1,
+                "run_parallel": False,
+                "size": 100
+            },
+            "keys": {
+                "pk_id": {
+                    "columns": ["Id"],
+                    "type": "PK"
+                }
+            },
+            "source": "..\\data\\pk_test.csv"
+        }
+    }
+
+    # Mock the open function, yaml.load, and validate_schema
+    with patch('builtins.open', mock_open(read_data='data')) as m:
+        with patch('yaml.load') as mock_yaml_load:
+            with patch('syngen.ml.validation_schema.validate_schema') as mock_validate_schema:
+                mock_yaml_load.return_value = expected_metadata
+                metadata = loader.load_data('tests/unit/data_loaders/fixtures/metadata/metadata.yaml')
+
+                # Assert that validate_schema was called with the correct arguments
+                # mock_validate_schema.assert_called_once_with(configuration_schema, expected_metadata)
+                assert metadata == expected_metadata
+
+
+def test_load_metadata_by_yaml_loader_in_yml_format():
+    loader = YAMLLoader()
+
+    expected_metadata = {
+        "pk_test": {
+            "train_settings": {
+                "drop_null": False,
+                "epochs": 1,
+                "print_report": False,
+                "row_limit": 800
+            },
+            "infer_settings": {
+                "print_report": True,
+                "random_seed": 1,
+                "run_parallel": False,
+                "size": 100
+            },
+            "keys": {
+                "pk_id": {
+                    "columns": ["Id"],
+                    "type": "PK"
+                }
+            },
+            "source": "..\\data\\pk_test.csv"
+        }
+    }
+
+    # Mock the open function, yaml.load, and validate_schema
+    with patch('builtins.open', mock_open(read_data='data')) as m:
+        with patch('yaml.load') as mock_yaml_load:
+            with patch('syngen.ml.validation_schema.validate_schema') as mock_validate_schema:
+                mock_yaml_load.return_value = expected_metadata
+                metadata = loader.load_data('tests/unit/data_loaders/fixtures/metadata/metadata.yml')
+
+                # Assert that validate_schema was called with the correct arguments
+                # mock_validate_schema.assert_called_once_with(configuration_schema, expected_metadata)
+                assert metadata == expected_metadata
+
+
 def test_save_metadata_in_yaml_format(test_yaml_path, test_df):
     metadata_loader = MetadataLoader(test_yaml_path)
     assert isinstance(metadata_loader.metadata_loader, YAMLLoader)
@@ -266,6 +364,7 @@ def test_save_metadata_in_yaml_format(test_yaml_path, test_df):
         metadata_loader.save_data(test_yaml_path, test_df)
         assert str(error) == "Saving YAML files is not supported"
 
+
 def test_save_metadata_in_yml_format(test_yml_path, test_df):
     metadata_loader = MetadataLoader(test_yml_path)
     assert isinstance(metadata_loader.metadata_loader, YAMLLoader)
@@ -273,5 +372,3 @@ def test_save_metadata_in_yml_format(test_yml_path, test_df):
     with pytest.raises(NotImplementedError) as error:
         metadata_loader.save_data(test_yml_path, test_df)
         assert str(error) == "Saving YAML files is not supported"
-
-
