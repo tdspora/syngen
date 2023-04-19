0cab395b26fb1ead7ff60f4f0f507fde50526dd0
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-04-19 17:01:16 +0300

    update unit tests

--- src/tests/unit/utils/test_utils.py
+++ src/tests/unit/utils/test_utils.py
@@ -1,31 +1,40 @@
-from unittest import TestCase
+import pytest
 from unittest.mock import Mock
 from syngen.ml.utils import slugify_attribute, slugify_parameters
 
 
-class TestSlugify(TestCase):
-    def test_slugify_attribute(self):
-        mock = Mock(
-            attr_1="My Test Attribute",
-            attr_2="Мой другой аттрибут",
-            attr_3="@#$12345*&^"
-        )
+def test_slugify_attribute(rp_logger):
+    mock = Mock(
+        attr_1="My Test Attribute",
+        attr_2="Мой другой аттрибут",
+        attr_3="@#$12345*&^"
+    )
 
-        @slugify_attribute(attr_1="slug_attr1", attr_2="slug_attr2", attr_3="slug_attr3")
-        def dummy_function(mock):
-            pass
+    @slugify_attribute(attr_1="slug_attr1", attr_2="slug_attr2", attr_3="slug_attr3")
+    def dummy_function(mock):
+        pass
 
-        dummy_function(mock)
+    dummy_function(mock)
 
-        assert mock.slug_attr1, "my-test-attribute"
-        assert mock.slug_attr2, "moi-drugoi-attribut"
-        assert mock.slug_attr3, "12345"
+    rp_logger.info(f"Slugifying the attributes of the mock object - {mock.attr_1, mock.attr_2, mock.attr_3}")
+    assert mock.slug_attr1 == "my-test-attribute"
+    assert mock.slug_attr2 == "moi-drugoi-attribut"
+    assert mock.slug_attr3 == "12345"
+    rp_logger.info("Test passed successfully")
 
-    def test_slugify_parameters(self):
-        @slugify_parameters("name")
-        def dummy_function(name):
-            return name
 
-        assert dummy_function(name="My Test Attribute"), "my-test-attribute"
-        assert dummy_function(name="Мой другой аттрибут"), "moi-drugoi-attribut"
-        assert dummy_function(name="@#$12345*&^"), "12345"
+@pytest.mark.parametrize(
+    "parameter, expected_parameter", [
+        ("My Test Attribute", "my-test-attribute"),
+        ("Мой другой аттрибут", "moi-drugoi-attribut"),
+        ("@#$12345*&^", "12345")
+    ]
+)
+def test_slugify_parameters(parameter, expected_parameter, rp_logger):
+    rp_logger.info(f"Slugifying the parameter - {parameter}")
+    @slugify_parameters("name")
+    def dummy_function(name):
+        return name
+
+    assert dummy_function(name=parameter), expected_parameter
+    rp_logger.info("Test passed successfully")
