32c902d69d80c78df7d81db33b986a9ba8c4fc88
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-05-22 10:31:25 +0300

    update 'VERSION', refactor the code

--- src/syngen/ml/validation_schema/__init__.py
+++ src/syngen/ml/validation_schema/__init__.py
@@ -1,2 +1,5 @@
-from syngen.ml.validation_schema.validation_schema import validate_schema
-from syngen.ml.validation_schema.validation_schema import configuration_schema
+from syngen.ml.validation_schema.validation_schema import (
+    validate_schema,
+    configuration_schema,
+    keys_schema
+)
