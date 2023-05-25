75bea3d9c4fe66f14c59916d84b1c1b0c1cac4a4
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-05-25 16:36:18 +0300

    create 'ml/custom_logger' in order to set up the certain logging level for training or inference processes

---
+++ src/syngen/ml/custom_logger/custom_logger.py
@@ -0,0 +1,25 @@
+import sys
+
+from loguru import logger
+
+class SingletonLogger:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(SingletonLogger, cls).__new__(cls)
+
+            cls._instance.logger = logger
+            cls._instance.logger.remove()
+
+            handler = {
+                "sink": sys.stdout,
+                "level": args[0]
+            }
+            cls._instance.logger.add(**handler)
+
+        return cls._instance
+
+
+def setup_logger(log_level: str):
+    return SingletonLogger(log_level)
