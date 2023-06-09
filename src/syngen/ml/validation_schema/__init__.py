46d9aed13ea89bedfc8fc3345a572201eea561ee
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-06-09 16:58:39 +0300

    support 'global' settings in the metadata file

--- src/syngen/ml/validation_schema/__init__.py
+++ src/syngen/ml/validation_schema/__init__.py
@@ -1,5 +1,1 @@
-from syngen.ml.validation_schema.validation_schema import (
-    validate_schema,
-    configuration_schema,
-    keys_schema
-)
+from syngen.ml.validation_schema.validation_schema import validate_schema
