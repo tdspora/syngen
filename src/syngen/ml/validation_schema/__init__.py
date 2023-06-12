6d226311b036014402f62d92ac2bd5924546e5f6
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-06-12 18:58:32 +0300

    minor changes in 'ml/validation_schema'

--- src/syngen/ml/validation_schema/__init__.py
+++ src/syngen/ml/validation_schema/__init__.py
@@ -1,1 +1,4 @@
-from syngen.ml.validation_schema.validation_schema import validate_schema
+from syngen.ml.validation_schema.validation_schema import (
+    validate_schema,
+    build_keys_schema
+)
