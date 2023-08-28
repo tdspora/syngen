57a2a9369ab9e9493749d1a427c6e56c11d003c4
Hanna Imshenetska <hanna_imshenetska@epam.com>
2023-08-28 16:44:35 +0300

    fix the order of columns in the generated data

--- src/syngen/ml/vae/models/dataset.py
+++ src/syngen/ml/vae/models/dataset.py
@@ -79,7 +79,7 @@ class Dataset:
         self.uuid_columns_types = {}
         self.dropped_columns = fetch_training_config(self.paths["train_config_pickle_path"]).dropped_columns
         self.non_existent_columns = set()
-        self.order_of_columns = self.df.columns.tolist()
+        self.order_of_columns = fetch_training_config(self.paths["train_config_pickle_path"]).columns
 
     def __set_pk_key(self, config_of_keys: Dict):
         """
