52c92f817eccb60ddc91e837dd6307aab8956367
Sergei Kudrenko <sergei_kudrenko@epam.com>
2023-12-15 14:18:13 +0000

    Refactor file uploader and training process

--- src/syngen/streamlit_app.py
+++ src/syngen/streamlit_app.py
@@ -21,22 +21,25 @@ if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
 st.title("Syngen")
-st.write("Upload CSV files and train your model.")
+st.write("Upload a CSV file and train your model.")
 
-# Define file uploader
-uploaded_files = st.file_uploader("Upload CSV files", type="csv", accept_multiple_files=False)
+# Define file uploader (for a single file)
+uploaded_file = st.file_uploader("Upload CSV file", type="csv", accept_multiple_files=False)
 
-dataframes = {}
-if uploaded_files:
-    for uploaded_file in uploaded_files:
-        # Save each uploaded file to the local directory
-        file_path = os.path.join(UPLOAD_DIRECTORY, uploaded_file.name)
-        with open(file_path, 'wb') as f:
-            f.write(uploaded_file.getvalue())
+# Preparing a single uploaded file for processing
+dataframe = {}
+if uploaded_file:
+    # Save the uploaded file to the local directory
+    file_path = os.path.join(UPLOAD_DIRECTORY, uploaded_file.name)
+    with open(file_path, 'wb') as f:
+        f.write(uploaded_file.getvalue())
 
-        df = pd.read_csv(file_path)
-        dataframes[uploaded_file.name] = df
-        st.write(f"Preview of {uploaded_file.name}:", df.head())
+    # Read the CSV into a DataFrame
+    df = pd.read_csv(file_path)
+    dataframe[uploaded_file.name] = df
+
+    # Show a preview of the uploaded file
+    st.write(f"Preview of {uploaded_file.name}:", df.head())
 
 # Main page for training parameters
 epochs = st.number_input('Epochs', min_value=1, value=10)
@@ -44,31 +47,29 @@ row_limit = st.number_input('Row Limit', min_value=1, max_value=None, value=1000
 
 @logger.catch
 def train_model():
-    with st.spinner('Training is in progress...'):
-        logger.info("Starting model training...")
-        for uploaded_file in uploaded_files:
-            file_path = os.path.join(UPLOAD_DIRECTORY, uploaded_file.name)
-            settings = {
-                "source": file_path,
-                "epochs": epochs,
-                "drop_null": False,  # Assuming this needs to remain static
-                "row_limit": row_limit,  # Using the row_limit from the main page
-                "batch_size": 32,  # Assuming this needs to remain static
-                "print_report": False  # Assuming this needs to remain static
-            }
-            worker = Worker(
-                table_name=uploaded_file.name,
-                settings=settings,
-                metadata_path=None,
-                log_level='DEBUG',
-                type_of_process="train"
-            )
-            worker.launch_train()
-        logger.info("Model training completed.")
+    logger.info("Starting model training...")
+    file_path = os.path.join(UPLOAD_DIRECTORY, uploaded_file.name)
+    settings = {
+        "source": file_path,
+        "epochs": epochs,
+        "drop_null": False,
+        "row_limit": row_limit,
+        "batch_size": 32,
+        "print_report": False
+    }
+    worker = Worker(
+        table_name=uploaded_file.name,
+        settings=settings,
+        metadata_path=None,
+        log_level='DEBUG',
+        type_of_process="train"
+    )
+    worker.launch_train()
+    logger.info("Model training completed.")
 
 # Button to start the training process
 if st.button('Start Model Training'):
-    if uploaded_files:
+    if uploaded_file:
         training_thread = threading.Thread(target=train_model)
         training_thread.start()
 
@@ -83,7 +84,4 @@ if st.button('Start Model Training'):
                         st.success("Training completed.")
                         break
     else:
-        st.warning("Please upload CSV files to proceed.")
-
-# for the error related to Thread 'Thread-82': missing ScriptRunContext
-# check: https://stackoverflow.com/questions/64568709/python-use-concurrent-futures-in-streamlit-got-missing-reportcontext
+        st.warning("Please upload a CSV file to proceed.")
