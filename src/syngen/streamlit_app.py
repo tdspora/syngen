7970fa8b2f23904217bf575fa99a6dca9356e560
Sergei Kudrenko <sergei_kudrenko@epam.com>
2023-12-18 16:07:47 +0300

    Refactor file uploader and add data generation functionality

--- src/syngen/streamlit_app.py
+++ src/syngen/streamlit_app.py
@@ -11,8 +11,11 @@ import threading
 # Setup the log queue
 log_queue = queue.Queue()
 
+
 def log_sink(message):
     log_queue.put(message.record["message"])
+
+
 logger.add(log_sink)
 
 # Create the path for uploaded CSV files
@@ -21,13 +24,13 @@ if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
 st.title("Syngen")
-st.write("Upload a CSV file and train your model.")
+uploaded_file = st.file_uploader(
+    "Upload CSV file",
+    type="csv",
+    accept_multiple_files=False
+)
+dataframe = {}  # Define the "dataframe" variable as an empty dictionary
 
-# Define file uploader (for a single file)
-uploaded_file = st.file_uploader("Upload CSV file", type="csv", accept_multiple_files=False)
-
-# Preparing a single uploaded file for processing
-dataframe = {}
 if uploaded_file:
     # Save the uploaded file to the local directory
     file_path = os.path.join(UPLOAD_DIRECTORY, uploaded_file.name)
@@ -42,8 +45,12 @@ if uploaded_file:
     st.write(f"Preview of {uploaded_file.name}:", df.head())
 
 # Main page for training parameters
-epochs = st.number_input('Epochs', min_value=1, value=10)
-row_limit = st.number_input('Row Limit', min_value=1, max_value=None, value=1000)
+epochs = st.number_input('Epochs', min_value=1, value=1)
+size_limit = st.number_input('Size Limit',
+                             min_value=1,
+                             max_value=None,
+                             value=1000)
+
 
 @logger.catch
 def train_model():
@@ -52,8 +59,8 @@ def train_model():
     settings = {
         "source": file_path,
         "epochs": epochs,
+        "row_limit": 1000,
         "drop_null": False,
-        "row_limit": row_limit,
         "batch_size": 32,
         "print_report": False
     }
@@ -67,22 +74,66 @@ def train_model():
     worker.launch_train()
     logger.info("Model training completed.")
 
-# Button to start the training process
-if st.button('Start Model Training'):
+
+def infer_model():
+    try:
+        logger.info("Starting data generation...")
+        settings = {
+            "size": size_limit,
+            "batch_size": 32,
+            "run_parallel": False,
+            "random_seed": None,
+            "print_report": False
+        }
+        worker = Worker(
+            table_name=uploaded_file.name,
+            settings=settings,
+            metadata_path=None,
+            log_level='DEBUG',
+            type_of_process="infer"
+        )
+        worker.launch_infer()
+        logger.info("Data generation completed.")
+    except Exception as e:
+        logger.error(f"Error during infer: {e}")
+        log_queue.put(f"Error during infer: {e}")
+
+
+# Function to run both training and inference in sequence
+
+def train_and_infer():
+    train_model()
+    infer_model()
+
+
+# Button to start the training and inference process
+if st.button('Generate data'):
     if uploaded_file:
-        training_thread = threading.Thread(target=train_model)
-        training_thread.start()
+        # Start the training and inference thread
+        thread = threading.Thread(target=train_and_infer)
+        thread.start()
+
+        # Display logs from the log queue in different format on streamlit
+#        with st.spinner('Waiting for the training to complete...'):
+#            with st.expander("Logs"):
+#                while True:
+#                    if not log_queue.empty():
+#                        with st.code("logs"):
+#                            st.text(log_queue.get())
+#                    elif not training_thread.is_alive():
+#                        st.success("Training completed.")
+#                        break
+#                    time.sleep(0.001)
 
         # Display logs from the log queue
-        with st.spinner('Waiting for the training to complete...'):
+        with st.spinner('Waiting for the processes to complete...'):
             with st.expander("Logs"):
-                while True:
+                st.code("Logs will be displayed here...")
+                while thread.is_alive():
                     if not log_queue.empty():
-                        with st.code("logs"):
-                            st.text(log_queue.get())
-                    elif not training_thread.is_alive():
-                        st.success("Training completed.")
-                        break
-                    time.sleep(0.01)
+                        log = log_queue.get()
+                        st.code(log, language='log')
+                    time.sleep(0.001)
+                st.success("Data generation completed.")
     else:
         st.warning("Please upload a CSV file to proceed.")
