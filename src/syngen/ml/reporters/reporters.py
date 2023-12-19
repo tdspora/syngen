7bfcb79abce65dd05d2f776d884131bb3cd17d53
Hanna Imshenetska <69158110+Anna050689@users.noreply.github.com>
2023-12-19 17:15:19 +0200

    fix the process of the conversion from a datetime to a timestamp (#335)
    * fix the handling of dates during the conversion from datetime to timestamp
    
    * update unit tests in 'tests/unit/dataset/test_dataset.py'
    
    * refactor the code related to issues detected by 'flake8', refactor the method 'datetime_to_timestamp' in 'syngen/ml/utils', update unit tests
    
    * update 'setup.cfg'
    
    * minor changes in 'ml/reporters'
    
    * refactor the method '_avro_data_pipeline' of the class Dataset, update 'VERSION'
    
    * update 'VERSION'
    
    * add the attribute to the class Dataset, refactor the method 'preprocess_data' in the class Reporter
    
    * update 'VERSION'
    
    * refactor the method 'convert_to_timestamp' of the class Reporter, update unit tests
    
    * update 'VERSION', update unit tests

--- src/syngen/ml/reporters/reporters.py
+++ src/syngen/ml/reporters/reporters.py
@@ -46,6 +46,15 @@ class Reporter:
         )
         return types
 
+    @staticmethod
+    def convert_to_timestamp(df, col_name, date_format, na_values):
+        """
+        Convert date column to timestamp
+        """
+        return [
+            datetime_to_timestamp(d, date_format) if d not in na_values else np.NaN for d in df[col_name]
+        ]
+
     def preprocess_data(self):
         """
         Preprocess original and synthetic data.
@@ -58,6 +67,7 @@ class Reporter:
         columns_nan_labels = get_nan_labels(original)
         original = nan_labels_to_float(original, columns_nan_labels)
         synthetic = nan_labels_to_float(synthetic, columns_nan_labels)
+        dataset = fetch_dataset(self.paths["dataset_pickle_path"])
         types = self.fetch_data_types()
         (
             str_columns,
@@ -70,11 +80,10 @@ class Reporter:
         ) = types
         original = original[[col for col in original.columns if col in set().union(*types)]]
         synthetic = synthetic[[col for col in synthetic.columns if col in set().union(*types)]]
-        for date_col in date_columns:
-            original[date_col] = list(map(lambda d: datetime_to_timestamp(d), original[date_col]))
-            synthetic[date_col] = list(
-                map(lambda d: datetime_to_timestamp(d), synthetic[date_col])
-            )
+        na_values = dataset.format.get("na_values", [])
+        for date_col, date_format in dataset.date_mapping.items():
+            original[date_col] = self.convert_to_timestamp(original, date_col, date_format, na_values)
+            synthetic[date_col] = self.convert_to_timestamp(synthetic, date_col, date_format, na_values)
 
         int_columns = date_columns | int_columns
         text_columns = str_columns | long_text_columns
