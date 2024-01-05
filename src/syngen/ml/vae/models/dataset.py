7c442b3d6c8e73b1a375cca5d86e2b99c391259c
Hanna Imshenetska <hanna_imshenetska@epam.com>
2024-01-05 18:49:22 +0200

    add the method 'clean_up_metadata' in 'ml/utils', update the method '__getstate__' of the class Dataset

--- src/syngen/ml/vae/models/dataset.py
+++ src/syngen/ml/vae/models/dataset.py
@@ -30,7 +30,7 @@ from syngen.ml.utils import (
 )
 from syngen.ml.data_loaders import DataLoader
 from syngen.ml.utils import slugify_parameters
-from syngen.ml.utils import fetch_training_config
+from syngen.ml.utils import fetch_training_config, clean_up_metadata
 
 
 @dataclass
@@ -62,6 +62,7 @@ class Dataset:
         """
         Return a dictionary of the dataset's state
         """
+        clean_up_metadata(self.metadata)
         dataset_instance = self.__dict__.copy()
         attribute_keys_to_remove = ["df", "non_existent_columns"]
 
