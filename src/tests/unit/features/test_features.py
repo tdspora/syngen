43a33a597d2461766aec19996a39dd697fdda9e1
Roman Nemuchynskyi <roman_nemuchynskyi@epam.com>
2024-08-15 17:40:45 +0300

    Tests for top_p filtering and top_k filtering are added

--- src/tests/unit/features/test_features.py
+++ src/tests/unit/features/test_features.py
@@ -2,6 +2,7 @@ import pytest
 
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 from syngen.ml.vae.models.features import BaseFeature, CharBasedTextFeature
 from tests.conftest import SUCCESSFUL_MESSAGE
@@ -63,3 +64,43 @@ def test_inverse_transform_of_char_based_text_feature(rp_logger):
     assert len(result) == 20
     assert np.mean([len(i) for i in result]) == 4
     rp_logger.info(SUCCESSFUL_MESSAGE)
+
+def test_top_k_filtering(rp_logger):
+    rp_logger.info(
+        "Testing the method 'top_k_top_p_filtering' of the class CharBasedTextFeature in top_k mode"
+    )
+    feature = CharBasedTextFeature(
+        name="text_column",
+        text_max_len=4
+    )
+    data = np.loadtxt(
+        "tests/unit/features/fixtures/tensor.csv"
+    ).reshape((20, 4, 12)).astype(np.float32)
+    result = feature.top_k_top_p_filtering(data, top_k=3, filter_value=0.0)
+    ethalon = np.loadtxt(
+        "tests/unit/features/fixtures/top_k-tensor.csv"
+    ).reshape((20, 4, 12)).astype(np.float32)
+    np.testing.assert_array_equal(result, ethalon)
+
+
+def test_top_p_filtering(rp_logger):
+    rp_logger.info(
+        "Testing the method 'top_k_top_p_filtering' of the class CharBasedTextFeature in top_p mode"
+    )
+    feature = CharBasedTextFeature(
+        name="text_column",
+        text_max_len=4
+    )
+    data = np.loadtxt(
+        "tests/unit/features/fixtures/tensor.csv"
+    ).reshape((20, 4, 12)).astype(np.float32)
+
+    result = tf.nn.softmax(
+        np.stack(
+            [feature.top_k_top_p_filtering(matrix, top_p=0.7) for matrix in data],
+            axis=0),
+        axis=-1).numpy()
+    ethalon = np.loadtxt(
+        "tests/unit/features/fixtures/top_p-tensor.csv"
+    ).reshape((20, 4, 12)).astype(np.float32)
+    np.testing.assert_array_equal(result, ethalon)
