e8fc971bc03f88753766dbe9de29f3626e2d6ef7
Hanna Imshenetska <69158110+Anna050689@users.noreply.github.com>
2024-09-04 11:52:41 +0300

    add gitaction to launch unit tests (#453)
    * temporary changes
    
    * refactor the class Dataset
    
    * refactor the class Dataset
    
    * revert changes in the class Dataset
    
    * temporary changes in 'tests/unit/dataset'
    
    * update unit tests, refactor the class Dataset
    
    * minor changes in 'tests/unit/dataset'
    
    * update unit tests
    
    * update unit tests, fix issues raised by 'flake8'
    
    * refactor 'ml/data_loaders'
    
    * refactor the code
    
    * Update action-build-deploy.yml
    
    * Update action-build-deploy.yml
    
    * update workflow pytests
    
    * revert changes due to the future implementations in unit testing
    
    * fix paths to fixture files
    
    * Update VERSION
    
    * revert changes in 'ml/config'
    
    * revert changes
    
    * revert changes
    
    * revert changes
    
    * revert changes
    
    * update unit tests
    
    ---------
    
    Co-authored-by: Hanna Imshenetska <Hanna_Imshenetska@epam.com@EVZZAMZSA0021.epam.com>
    Co-authored-by: Sergio <43149357+serhio-k@users.noreply.github.com>

--- src/tests/unit/launchers/test_launch_train.py
+++ src/tests/unit/launchers/test_launch_train.py
@@ -3,11 +3,11 @@ from click.testing import CliRunner
 
 from syngen.train import launch_train
 from syngen.ml.worker import Worker
-from tests.conftest import SUCCESSFUL_MESSAGE
+from tests.conftest import SUCCESSFUL_MESSAGE, DIR_NAME
 
 TABLE_NAME = "test_table"
-PATH_TO_TABLE = "./tests/unit/launchers/fixtures/table_with_data.csv"
-PATH_TO_METADATA = "./tests/unit/launchers/fixtures/metadata.yaml"
+PATH_TO_TABLE = f"{DIR_NAME}/unit/launchers/fixtures/table_with_data.csv"
+PATH_TO_METADATA = f"{DIR_NAME}/unit/launchers/fixtures/metadata.yaml"
 
 
 @patch.object(Worker, "launch_train")
