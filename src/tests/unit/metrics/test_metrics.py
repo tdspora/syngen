51406f71cadfacfa159335e185a163d89ae94942
Ijka <toijka@gmail.com>
2024-10-09 09:44:34 +0000

    small logs changes

--- src/tests/unit/metrics/test_metrics.py
+++ src/tests/unit/metrics/test_metrics.py
@@ -15,6 +15,8 @@ def test_clustering_calculate_all(rp_logger):
     path_to_original = f"{DIR_NAME}/unit/metrics/fixtures/clusters-2.csv"
     path_to_synthetic = f"{DIR_NAME}/unit/metrics/fixtures/synthetic_clusters-2_10k.csv"
 
+    threshold = 0.95
+
     original = pd.read_csv(path_to_original)
     synthetic = pd.read_csv(path_to_synthetic)
 
@@ -25,7 +27,6 @@ def test_clustering_calculate_all(rp_logger):
 
     mean_score = clustering.calculate_all(categ_columns, cont_columns)
 
-    threshold = 0.95
-    assert mean_score >= threshold, f"Mean score should not less than {threshold}"
+    assert mean_score >= threshold, f"Mean score shouldn't be less than {threshold}"
 
     rp_logger.info(SUCCESSFUL_MESSAGE)
