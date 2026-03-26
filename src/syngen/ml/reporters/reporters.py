acac109695a7ca8a08c4e184ba254ab4f927c173
hanna_imshenetska <hanna_imshenetska@epam.com>
2026-03-26 10:32:02 +0000

    refactor 'ml/reporters'

--- src/syngen/ml/reporters/reporters.py
+++ src/syngen/ml/reporters/reporters.py
@@ -187,8 +187,8 @@ class Reporter:
         categorical_columns = categorical_columns | binary_columns
 
         for col in categorical_columns:
-            original[col] = original[col].astype(str).fillna("nan")
-            synthetic[col] = synthetic[col].astype(str).fillna("nan")
+            original[col] = original[col].astype(str)
+            synthetic[col] = synthetic[col].astype(str)
         return (
             original,
             synthetic,
