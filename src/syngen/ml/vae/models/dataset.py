72ca461c0876dbf73d09f87bbe3cab80dfe85279
Ijka <toijka@gmail.com>
2026-06-01 13:42:59 +0000

    minor change in logs

--- src/syngen/ml/vae/models/dataset.py
+++ src/syngen/ml/vae/models/dataset.py
@@ -292,7 +292,7 @@ class Dataset:
             not_all_na_mask = ~all_na_mask
             if self.df[not_all_na_mask][key_columns].duplicated().any():
                 warning_msg = (
-                    f"The unique key '{key_name}' contains duplicates."
+                    f"The unique key '{key_name}' contains duplicates. "
                 )
                 warnings.append(warning_msg)
 
